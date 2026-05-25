63444bca395722d6d5467271c74c54d35620f5a2
João Guilherme Stumpf Fernandes <guistumpf@users.noreply.github.com>
2026-05-25 11:25:05 -0300

    layout e responsivo

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,14 @@ import Link from "next/link";
 
 export default function CardDemo() {
   return (
-   <>
+    <>
+    <Link href="/submissoes" className="fixed top-4 right-4 z-50">
+    submissoes
+  </Link>
+   
 
-   <Link href={"/submissoes"} className="navtext">submissoes</Link>
-   <div className="flex justify-center items-center h-screen">
-
-    <Card className="w-full max-w-sm">
+    <div className="flex justify-center items-center h-dvh">
+    <Card className="w-full max-w-sm mx-4">
       <CardHeader>
         <CardTitle>Média de Idade</CardTitle>
         <CardDescription>
@@ -38,7 +40,7 @@ export default function CardDemo() {
               <Label htmlFor="email">Nome / Apelido</Label>
               <Input
                 id="email"
-                placeholder="Opcional"
+                placeholder=" Opcional"
                 required
                 className="border white"
                 />
@@ -47,7 +49,7 @@ export default function CardDemo() {
               <div className="flex items-center">
                 <Label htmlFor="password">Idade</Label>
                </div>
-              <Input id="password"  placeholder="Ex: 30" required className="border white"/>
+              <Input id="password"  placeholder=" Ex: 30" required className="border white"/>
             </div>
           </div>
         </form>
@@ -60,6 +62,7 @@ export default function CardDemo() {
       </CardFooter>
     </Card>
                   </div>
+               
                 </>
   )
 }
