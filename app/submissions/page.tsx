65586c685298f031abcdfe2e6743ada2dac4c49a
João Guilherme Stumpf Fernandes <guistumpf@users.noreply.github.com>
2026-05-29 23:18:43 -0300

    fix on sub too

--- app/submissions/page.tsx
+++ app/submissions/page.tsx
@@ -1,6 +1,7 @@
 import Submissoes from "../components/page";
 import BackButton from "../components/backbutton";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default function SubPage() {
     return (
         <Submissoes>
